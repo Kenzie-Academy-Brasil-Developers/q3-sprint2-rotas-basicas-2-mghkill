980f58992fd0d539f77bb3f47207a5103b7e74eb
Filipe Melo <filipexpert63@gmail.com>
2022-02-22 17:06:45 -0300

    Finalizad projetinho GET time

--- rotas_basicas/app.py
+++ rotas_basicas/app.py
@@ -1,1 +1,29 @@
-# Seu código aqui
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+time = int(datetime.now().strftime("%H"))
+
+@app.route('/')
+def home():
+    
+    return {"data": "Hello Flask!"}
+
+@app.route('/current_datetime')
+def current_datetime():
+    
+    msg = {}
+    if(time < 12):
+        msg = {"current_datetime": datetime.now(),"message": "Bom dia!"}
+    elif(time > 12) and (time < 18):
+        msg = {"current_datetime": datetime.now(),"message": "Boa tarde!"}
+    else:
+        msg = {"current_datetime": datetime.now(),"message": "Boa noite!"}
+    
+    return msg
+
+
+    
+
+
